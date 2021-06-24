1eca3aa2f2ef607db14ed8e4c85293efe744a55b
nils0n <nilson_jose.v@hotmail.com>
2021-06-23 22:59:58 -0300

    aula2

--- assests/js/main.js
+++ assests/js/main.js
@@ -14,3 +14,17 @@ for (const link of links) {
     nav.classList.remove('show')
   })
 }
+
+//mudar o header da pag quando der scroll(rolar)
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
+//*/
