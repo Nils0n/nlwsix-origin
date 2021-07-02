c6af8b59563eb6342c7870a8b0d6b9a9939e07a1
nils0n <nilson_jose.v@hotmail.com>
2021-07-02 00:42:03 -0300

    changeJs

--- assests/js/main.js
+++ assests/js/main.js
@@ -17,17 +17,15 @@ for (const link of links) {
 
 //mudar o header da pag quando der scroll(rolar)
 
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
-
-window.addEventListener('scroll', function () {
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
-})
-//*/
+}
 
 const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
@@ -52,6 +50,22 @@ scrollReveal.reveal(
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials,
   #contact .text, #contact .links
+  footer .brand, footer .social
 `,
   { interval: 100 }
 )
+
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+//WHEN SCROLL
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
